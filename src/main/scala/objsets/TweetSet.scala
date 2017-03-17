ceff6c66e42413867fbcab91419cc9e3d6912465
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2017-03-17 18:12:14 +0100

    Work on Google Apple

--- src/main/scala/objsets/TweetSet.scala
+++ src/main/scala/objsets/TweetSet.scala
@@ -247,14 +247,27 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+
+  lazy val googleTweets: TweetSet = {
+    TweetReader.allTweets.filter(x => isContained(x, google))
+  }
+
+  def isContained(tweet: Tweet, list: List[String]): Boolean = {
+    list.exists(listElement => {
+      listElement.contains(tweet.text)
+    })
+  }
+
+
+  lazy val appleTweets: TweetSet = {
+    TweetReader.allTweets.filter(x => isContained(x, apple))
+  }
 
   /**
     * A list of all tweets mentioning a keyword from either apple or google,
     * sorted by the number of retweets.
     */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
